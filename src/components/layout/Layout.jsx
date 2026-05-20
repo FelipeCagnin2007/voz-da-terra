f3f48c57ff2fa24f9ba19a924e24146668863084
FelipeCagnin2007 <cagnin.lima.2007@gmail.com>
2026-05-19 21:50:13 -0300

    A PORRA DO DISPLAY FLEX

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -40,19 +40,17 @@ export const Layout = ({ children }) => {
   const SidebarContent = () => (
     <>
       <div className="sidebar-top">
-        <div className="sidebar-header">
-          <div className="logo">
-            <img src="/logo.png" alt="logo" />
-            <div>VOZ DA<br />TERRA</div>
-          </div>
-
-          <button
-            className={`hamburger ${isMenuOpen ? 'active' : ''}`}
-            onClick={() => setIsMenuOpen(!isMenuOpen)}
-          >
-            <span></span><span></span><span></span>
-          </button>
+        <div className="logo">
+          <img src="/logo.png" alt="logo" />
+          <div>VOZ DA<br />TERRA</div>
         </div>
+
+        <button
+          className={`hamburger ${isMenuOpen ? 'active' : ''}`}
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        >
+          <span></span><span></span><span></span>
+        </button>
 
         <nav className={`nav-menu ${isMenuOpen ? 'active' : ''}`}>
           <ul>
